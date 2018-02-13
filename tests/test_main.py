b2c886933feb14585ff0bfb0c273b814eb78c35e
anan44 <t95371@gmail.com>
2018-02-13 21:27:54 +0100

    mended a issue with a test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,6 @@ class TestMain(unittest.TestCase):
         config = main.read_config("config_template.json")
 
         self.assertEqual(config["api_key"], "Your API key goes here")
-        self.assertEqual(config["polling_interval_in_minutes"], 5)
+        self.assertEqual(config["polling_interval_in_minutes"], 180)
         self.assertEqual(config["locations"][0]["name"], "Vantaa")
         self.assertEqual(config["days_checked"], 5)
